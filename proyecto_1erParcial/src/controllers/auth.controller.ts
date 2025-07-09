156751bf0868f379debb99de888fdaaa66c3ac66
Hugo Perez <hugoperez510@hotmail.com>
2025-07-09 11:54:00 -0600

    Modificar username por email

--- proyecto_1erParcial/src/controllers/auth.controller.ts
+++ proyecto_1erParcial/src/controllers/auth.controller.ts
@@ -2,22 +2,22 @@ import { Request, Response } from "express";
 import NodeCache from "node-cache";
 import { generateAccessToken } from "../utils/generateToken";
 
- //Inicializa el caché de Node
-const cache= new NodeCache();
+//Inicializa el caché de Node
+const cache = new NodeCache();
 
 export const login = (req: Request, res: Response): void => {
-    const { username, password } = req.body;
+  const { email, password } = req.body;
 
-    // Simulate user authentication
-    if (username!=="admin" || password!=="1234") {
-        res.status(401).json({ message: "Invalid credentials" });
-        return;
-    }
+  // Simulate user authentication
+  if (email !== "admin@example.com" || password !== "1234") {
+    res.status(401).json({ message: "Invalid credentials" });
+    return;
+  }
 
-    const userId="123456";
-    const accessToken = generateAccessToken(userId);
+  const userId = "123456";
+  const accessToken = generateAccessToken(userId);
 
-    //Agregar Token
-    cache.set(userId, accessToken, 60*15);
-    res.json({accessToken});
+  //Agregar Token
+  cache.set(userId, accessToken, 60 * 15);
+  res.json({ accessToken });
 };
